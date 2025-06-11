0f08b7bd6551a8489ecf71b2772ab5c1f2ea3abf
Nick <ncote@ucar.edu>
2025-06-10 22:28:53 -0600

    change to ingress URL

--- whokan/frontend/assets/js/api.js
+++ whokan/frontend/assets/js/api.js
@@ -6,7 +6,7 @@
  */
 
 class ApiService {
-    constructor(baseUrl = '/api/v1') {  // Changed to relative path
+    constructor(baseUrl = 'https://whokan.nickknows.net/api/v1') {  // Changed to relative path
         this.baseUrl = baseUrl;
         this.token = localStorage.getItem('token');
     }
